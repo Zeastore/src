dd1e19e28c925176789ad000ac8a2cce7561773f
Zea King Store <98459024+Zeastore@users.noreply.github.com>
2022-10-04 02:21:55 +0700

    Update about.sh

--- about.sh
+++ about.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 clear
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "\E[44;1;39m      ⇱ UMANITY PROJECT ⇲                      \E[0m"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "# For Debian 9 & Debian 10 64 bit               #"
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
 echo -e "# For VPS with KVM and VMWare virtualization    #"
@@ -18,7 +18,7 @@ echo -e "#-----------------------------------------------#"
 echo -e "# Created By : HAMBA ALLAH                      #"
 echo -e "# Contact    : SILAKAN SHOLAWAT                 #"
 echo -e "# Whatsapp   : SEDEKAH YANG BANYAK              #"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "\E[44;1;39m      ⇱ UMANITY PROJECT ⇲                      \E[0m"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 
