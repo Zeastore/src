446bdf184eca007e0979138aa4e1df972a2db523
Zea King Store <98459024+Zeastore@users.noreply.github.com>
2022-10-04 02:14:11 +0700

    Update about.sh

--- about.sh
+++ about.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+clear
+echo -e "================================================="
+echo -e "#      Premium Auto Script By HAMBA ALLAH       #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# ALLAH SWT                                     #"
+echo -e "# NABI MUHAMMAD                                 #"
+echo -e "# KELUARGA KITA                                 #"
+echo -e "# TEMAN & GURU                                  #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Created By : HAMBA ALLAH                      #"
+echo -e "# Contact    : SILAKAN SHOLAWAT                 #"
+echo -e "# Whatsapp   : SEDEKAH YANG BANYAK              #"
+echo -e "================================================="
