ecf048a71ec2914e3caeb2cbafcf5e2d792e1b4e
Zea King Store <98459024+Zeastore@users.noreply.github.com>
2022-10-04 14:05:42 +0700

    Update cf.sh

--- ssh/cf.sh
+++ ssh/cf.sh
@@ -17,9 +17,9 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 #MYIP=$(wget -qO- https://ipv6.icanhazip.com);
 clear
 apt install jq curl -y
-DOMAIN=zeaking.my.id
+DOMAIN=zeaking-sc.me
 sub=$(</dev/urandom tr -dc a-z | head -c4)
-SUB_DOMAIN=${sub}.zeaking.my.id
+SUB_DOMAIN=${sub}.zeaking-sc.me
 CF_ID=mulahkual@gmail.com
 CF_KEY=1351e3252ad80b9a761d34d125545053403b5
 set -euo pipefail
