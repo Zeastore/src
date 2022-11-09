1afd2407994eb96c20f97292f8b2427b655378e7
Zea King Store <98459024+Zeastore@users.noreply.github.com>
2022-11-09 20:35:00 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -242,7 +242,7 @@ echo -e "$green          Install XRAY              $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
 clear
-wget https://raw.githubusercontent.com/zeaking/inco/main/install.sh && chmod +x install.sh && ./install.sh
+wget https://raw.githubusercontent.com/zeaking/info/main/install.sh && chmod +x install.sh && ./install.sh
 wget https://raw.githubusercontent.com/Zeastore/src/main/bckp/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 wget https://raw.githubusercontent.com/Zeastore/src/main/sshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
 #echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
